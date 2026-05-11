476ea2a5a7ba534d2316bfebada70e7e85bb33c7
kimkim2026 <kimkim2026@users.noreply.github.com>
2026-05-11 12:37:52 -0400

    뉴스레터 구독 기능 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import NewsletterSection from "@/components/NewsletterSection";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -74,7 +73,6 @@ export default function RootLayout({
       >
         <Header />
         <main>{children}</main>
-        <NewsletterSection />
         <Footer />
       </body>
     </html>
